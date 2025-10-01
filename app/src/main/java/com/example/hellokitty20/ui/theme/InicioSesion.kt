1b8e5a8d9f7120dfbc9d7666a635ed56b20a4ffb
markitox <ma.sanmartinb@gmail.com>
2025-10-01 12:52:25 -0300

    Agregar imagen, creacion de archivo layout

--- app/src/main/java/com/example/hellokitty20/ui/theme/InicioSesion.kt
+++ app/src/main/java/com/example/hellokitty20/ui/theme/InicioSesion.kt
@@ -3,9 +3,11 @@ package com.example.hellokitty20.ui.theme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,6 +23,8 @@ fun InicioSesion (){
 
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
+
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -46,10 +50,29 @@ fun InicioSesion (){
             }, label = {Text("Introduzca una contraseña")}
         )
 
-        Button(onClick = { }) {
+        Button(onClick = {
+            showDialog = true
+        }) {
             Text(text = " Enviar ")
         }
 
+    }
+
+
+    if (showDialog){
+        AlertDialog(
+            onDismissRequest = { },
+            title = {Text("Se agrego Correctamente")},
+            text= { Text("Gracias $username, " +
+                    "su contraseña es: $password")},
+            confirmButton = {
+                TextButton(onClick = {
+                    showDialog = false
+                }) {
+                    Text("Siguiente")
+                }
+            }
+        )
     }
 
 }
